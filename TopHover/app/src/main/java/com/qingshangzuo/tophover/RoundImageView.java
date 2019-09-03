9c2d30c8e9acc02d42245820b80032b17f17d6ab
luckilyswl <792411500@qq.com>
2019-09-03 11:30:36 +0800

    CoordinatorLayout的应用

---
+++ TopHover/app/src/main/java/com/qingshangzuo/tophover/RoundImageView.java
@@ -0,0 +1,14 @@
+package com.qingshangzuo.tophover;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoundImageView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.round_image_view);
+    }
+}
